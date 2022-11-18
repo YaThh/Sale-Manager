49a1ea5ca29a112896b4e1649d915fd565771c7d
unknown <giathuanvo2708@gmail.com>
2022-11-18 11:06:26 +0700

    first commit

---
+++ QLBH/Controllers/GiohangController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLBH.Models;
+
+namespace QLBH.Controllers
+{
+    public class GiohangController : Controller
+    {
+        qlbanhangEntities db = new qlbanhangEntities();
+        // GET: Giohang
+        public ActionResult Index()
+        {
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
+            return View(giohang);
+        }
+
+        public ActionResult AddToCart(string MaSP)
+        {
+            var giohang = Session["giohang"] as List<CartItem>;
+            var sanpham = db.SanPhams.FirstOrDefault(x => x.MaSP == MaSP);
+            var item = giohang.FirstOrDefault(x => x.MaSP == MaSP);
+            if (item != null)
+            {
+                item.SoLuong++;
+            }
+            else
+            {
+                CartItem newItem = new CartItem();
+                newItem.MaSP = MaSP;
+                newItem.TenSP = sanpham.TenSP;
+                newItem.SoLuong = 1;
+                newItem.DonGia = Convert.ToDouble(sanpham.Dongia);
+                giohang.Add(newItem);
+            }
+            Session["giohang"] = giohang;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update (string MaSP, int SoLuong)
+        {
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
+            CartItem item = giohang.FirstOrDefault(x => x.MaSP == MaSP);
+            if (item != null)
+            {
+                item.SoLuong = SoLuong;
+                Session["giohang"] = giohang;
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult DelCartItem (string MaSP, int SoLuong)
+        {
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
+            CartItem item = giohang.FirstOrDefault(x => x.MaSP == MaSP);
+            if (item != null)
+            {
+                giohang.Remove(item);
+                Session["giohang"] = giohang;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QLBH/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using QLBH.Models;
+
+namespace QLBH.Controllers
+{
+    public class HomeController : Controller
+    {
+        qlbanhangEntities db = new qlbanhangEntities();
+        public ActionResult Index()
+        {
+            var sanPham = db.SanPhams.Include(s => s.LoaiSP);
+            return View(sanPham.ToList());  
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
